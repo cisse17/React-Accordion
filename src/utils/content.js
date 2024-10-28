bccbc6a0759f100c6f69129f390c5a06f89f4512
cisse17 <119404406+cisse17@users.noreply.github.com>
2024-10-28 16:30:12 +0100

    learning how to do accordion with react

---
+++ src/utils/content.js
@@ -0,0 +1,22 @@
+export const accordionData = [
+    {
+        title : "What is HTML ?",
+        content : "The hyperText Markup Language or HTML is the standard ...."
+    },
+    {
+        title : "What is Python ?",
+        content : "The hyperText Markup Language or HTML is the standard ...."
+    },
+    {
+        title : "What is Django ?",
+        content : "The hyperText Markup Language or HTML is the standard ...."
+    },
+    {
+        title : "What is React ?",
+        content : "The hyperText Markup Language or HTML is the standard ...."
+    },
+    {
+        title : "What is JavaScripts ?",
+        content : "The hyperText Markup Language or HTML is the standard ...."
+    },
+]
